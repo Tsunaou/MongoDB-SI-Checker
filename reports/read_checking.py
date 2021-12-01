fbc0007fc4e2207bc392c45bbfdc15ff7f7d5cc6
young <895254752@qq.com>
2021-12-01 21:52:19 +0800

    Statistics

--- reports/read_checking.py
+++ reports/read_checking.py
@@ -1,5 +1,6 @@
 import re
 from files import *
+from scipy import stats
 
 
 class Result(object):
@@ -135,8 +136,10 @@ def new_result_map():
 
 
 if __name__ == '__main__':
-    filename = "wiredtiger/1131-amax/output1131-amax"
-    is_mongo = False
+    filename = "mongodb/1129-dell-opt/output-1129-dell-opt"
+    is_mongo = True
+    # filename = "wiredtiger/1131-dell/output-1131-dell"
+    # is_mongo = False
 
     with open(filename, "r") as f:
         outputs = list(map(lambda x: x.strip('\n'), f.readlines()))
@@ -183,7 +186,7 @@ if __name__ == '__main__':
             if "Exception" in line:
                 result.exception = True
             if "The error scale" in line:
-                result.error = float(re.findall(r"scale is (.+)seconds", line)[0])*1000
+                result.error = float(re.findall(r"scale is (.+)seconds", line)[0]) * 1000
 
         result.check()
         results.append(result)
@@ -204,37 +207,203 @@ if __name__ == '__main__':
     # save_obj_pickle(ok_result_map, "ok_result_map-1129.pickle")
     # save_obj_pickle(unknown_resut_map, "unknown_resut_map-1129.pickle")
 
+    # Fig. Perf
+    if is_mongo:
+        perf = {
+            'replica-set': {
+                60: {},
+                120: {},
+                180: {},
+                240: {},
+                300: {},
+            },
+            'sharded-cluster': {
+                60: {},
+                120: {},
+                180: {},
+                240: {},
+                300: {},
+                480: {}
+            }
+        }
+    else:
+        perf = {
+            'wiredtiger': {
+                10: {},
+                20: {},
+                30: {},
+                40: {},
+                50: {},
+                60: {},
+            },
+        }
+
+    # Fig avg perf
+    if is_mongo:
+        avg_perf = {
+            'replica-set': {
+                60: {},
+                120: {},
+                180: {},
+                240: {},
+                300: {},
+            },
+            'sharded-cluster': {
+                60: {},
+                120: {},
+                180: {},
+                240: {},
+                300: {},
+                480: {}
+            }
+        }
+    else:
+        avg_perf = {
+            'wiredtiger': {
+                10: {},
+                20: {},
+                30: {},
+                40: {},
+                50: {},
+                60: {},
+            },
+        }
+
+    # Fig stat
+    if is_mongo:
+        stat = {
+            4: {
+                60: {},
+                120: {},
+                180: {},
+                240: {},
+                300: {},
+                480: {}
+            },
+            8: {
+                60: {},
+                120: {},
+                180: {},
+                240: {},
+                300: {},
+                480: {}
+            },
+            12: {
+                60: {},
+                120: {},
+                180: {},
+                240: {},
+                300: {},
+                480: {}
+            }
+        }
+    else:
+        stat = {
+            4: {
+                10: {},
+                20: {},
+                30: {},
+                40: {},
+                50: {},
+                60: {},
+            },
+            8: {
+                10: {},
+                20: {},
+                30: {},
+                40: {},
+                50: {},
+                60: {},
+            },
+            12: {
+                10: {},
+                20: {},
+                30: {},
+                40: {},
+                50: {},
+                60: {},
+            }
+        }
+
     for deploy in ok_result_map.keys():
         deploy_data = ok_result_map.get(deploy)
         for txn_len in deploy_data.keys():
             txn_data = deploy_data.get(txn_len)
             for runtime in txn_data.keys():
                 check_data = txn_data.get(runtime)
-                all_count = 0
-                all_ok = 0
-                all_fail = 0
-                all_info = 0
-                all_time = 0
-                all_error = 0
+                all_count = []
+                all_ok = []
+                all_fail = []
+                all_info = []
+                all_time = []
+                all_error = []
 
                 for res in check_data:
                     # print(res)
-                    all_count = all_count + res.count
-                    all_ok = all_ok + res.ok
-                    all_fail = all_fail + res.fail
-                    all_info = all_info + res.info
-                    all_time = all_time + res.time
-                    all_error = all_error + res.error
+                    all_count.append(res.count)
+                    all_ok.append(res.ok)
+                    all_fail.append(res.fail)
+                    all_info.append(res.info)
+                    all_time.append(res.time)
+                    all_error.append(res.error)
 
                 n = len(check_data)
                 if n == 0:
                     continue
-                avg_count = all_count / n
-                avg_ok = all_ok / n
-                avg_fail = all_fail / n
-                avg_info = all_info / n
-                avg_time = all_time / n
-                avg_error = all_error / n
+                avg_count = sum(all_count) / n
+                avg_ok = sum(all_ok) / n
+                avg_fail = sum(all_fail) / n
+                avg_info = sum(all_info) / n
+                avg_time = sum(all_time) / n
+                avg_error = sum(all_error) / n
                 print("------- {} {} {}----------".format(deploy, txn_len, runtime))
+                # if deploy == "sharded-cluster" and runtime == 120 and txn_len == 4:
+                #     print("oks: ", all_ok)
+                #     print("times: ", all_time)
+                #     print([round(all_time[i] / all_ok[i], 3) for i in range(n)])
+
                 print("Check {} txns, avg_count={}, avg_ok={}, avg_info={}, avg_info={}, avg_time={}, error={}ms"
-                      .format(n, int(avg_count), int(avg_ok), int(avg_fail), int(avg_info), int(avg_time), round(avg_error, 3)))
+                      .format(n, int(avg_count), int(avg_ok), int(avg_fail), int(avg_info), int(avg_time),
+                              round(avg_error, 3)))
+                perf[deploy][runtime][txn_len] = avg_time
+                # avg_per_txn_list = [round(all_time[i] / all_ok[i], 8) for i in range(n)]
+                avg_per_txn = round(avg_time / avg_ok, 8)
+                avg_perf[deploy][runtime][txn_len] = avg_per_txn
+                print("Cost {}ms to per txn".format(avg_per_txn))
+
+                if deploy == "wiredtiger":
+                    stat[txn_len][runtime]['wt_all'] = avg_count
+                    stat[txn_len][runtime]['wt_ok'] = avg_ok
+                elif deploy == "replica-set":
+                    stat[txn_len][runtime]['rs_all'] = avg_count
+                    stat[txn_len][runtime]['rs_ok'] = avg_ok
+                else:
+                    stat[txn_len][runtime]['sc_all'] = avg_count
+                    stat[txn_len][runtime]['sc_ok'] = avg_ok
+
+    for deploy in perf.keys():
+        print(deploy)
+        deploy_stat = perf[deploy]
+        for runtime in deploy_stat.keys():
+            td = deploy_stat[runtime]
+            print("{} {} {} {}".format(runtime, td[4] / 1000, td[8] / 1000, td[12] / 1000))
+
+    for txn_len in stat.keys():
+        print("op", txn_len)
+        for runtime in stat[txn_len].keys():
+            td = stat[txn_len][runtime]
+            if runtime == 480:
+                td['rs_all'] = 0
+                td['rs_ok'] = 0
+            if is_mongo:
+                print("{} {} {} {} {}".format(runtime, td['rs_all'], td['rs_ok'], td['sc_all'], td['sc_ok']))
+            else:
+                print("{} {} {}".format(runtime, td['wt_all'], td['wt_ok']))
+
+    print("Avg time for each checking each transaction")
+    for deploy in avg_perf.keys():
+        print(deploy)
+        deploy_stat = avg_perf[deploy]
+        for runtime in deploy_stat.keys():
+            td = deploy_stat[runtime]
+            print("{} {} {} {}".format(runtime, td[4], td[8], td[12]))
