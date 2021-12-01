26dc2f42a0ef7e2d396e912cccf42817549b7ccb
young <895254752@qq.com>
2021-12-01 15:54:33 +0800

    Update read_checking.py

--- reports/read_checking.py
+++ reports/read_checking.py
@@ -30,6 +30,8 @@ class Result(object):
         # Output
         self.satisfy = False
         self.exception = False
+        # [INFO] The error scale is 0.012124595seconds
+        self.error = 0.0
 
     def set_count(self, count, ok, fail, info):
         self.count = count
@@ -41,11 +43,12 @@ class Result(object):
         self.satisfy = self.INT and self.EXT
 
     def __str__(self) -> str:
-        return "Checking {} {} count={}, ok={}, fail={}, info={}, time={}".format(self.type, self.satisfy, self.count,
-                                                                                  self.ok,
-                                                                                  self.fail,
-                                                                                  self.info,
-                                                                                  self.time)
+        if self.deployment == "replica-set" or self.deployment == "sharded-cluster":
+            return "Checking {} {} count={}, ok={}, fail={}, info={}, time={}" \
+                .format(self.type, self.satisfy, self.count, self.ok, self.fail, self.info, self.time)
+        else:
+            return "Checking {} {} count={}, ok={}, fail={}, info={}, time={}, error={} ms" \
+                .format(self.type, self.satisfy, self.count, self.ok, self.fail, self.info, self.time, self.error)
 
 
 def new_result_map():
@@ -98,7 +101,33 @@ def new_result_map():
                 300: [],
                 480: []
             }
-        }
+        },
+        "wiredtiger": {
+            4: {
+                10: [],
+                20: [],
+                30: [],
+                40: [],
+                50: [],
+                60: []
+            },
+            8: {
+                10: [],
+                20: [],
+                30: [],
+                40: [],
+                50: [],
+                60: []
+            },
+            12: {
+                10: [],
+                20: [],
+                30: [],
+                40: [],
+                50: [],
+                60: []
+            }
+        },
 
     }
 
@@ -106,7 +135,9 @@ def new_result_map():
 
 
 if __name__ == '__main__':
-    filename = "mongodb/1129/output1129"
+    filename = "wiredtiger/1131-amax/output1131-amax"
+    is_mongo = False
+
     with open(filename, "r") as f:
         outputs = list(map(lambda x: x.strip('\n'), f.readlines()))
 
@@ -125,11 +156,16 @@ if __name__ == '__main__':
             if "Checking history" in line:
                 result.dir = re.findall(r'Checking history for .+ at (.+)/history.edn', line)[0]
                 result.type = re.findall(r'Checking history for (.+) at', line)[0]
-                result.deployment = re.findall(r'mongodb wr (.+) w:majority', line)[0]
+                if is_mongo:
+                    result.deployment = re.findall(r'mongodb wr (.+) w:majority', line)[0]
+                    result.timeout = int(re.findall(r'timeout-txn:(\d+)', line)[0])
+                else:
+                    result.deployment = "wiredtiger"
+                    result.timeout = 5000
+
                 result.runtime = int(re.findall(r'time:(\d+)', line)[0])
-                result.timeout = int(re.findall(r'timeout-txn:(\d+)', line)[0])
                 result.txn_len = int(re.findall(r'txn-len:(\d+)', line)[0])
-                result.id = re.findall(r'snapshot partition/(.+)/history.edn', line)[0]
+                result.id = re.findall(r'/(.+)/history.edn', line)[0]
             if "valid" in line:
                 result.valid = True
             if line == "[INFO] Checking INT Successfully":
@@ -146,6 +182,8 @@ if __name__ == '__main__':
                 result.set_count(count, ok, fail, info)
             if "Exception" in line:
                 result.exception = True
+            if "The error scale" in line:
+                result.error = float(re.findall(r"scale is (.+)seconds", line)[0])*1000
 
         result.check()
         results.append(result)
@@ -172,12 +210,12 @@ if __name__ == '__main__':
             txn_data = deploy_data.get(txn_len)
             for runtime in txn_data.keys():
                 check_data = txn_data.get(runtime)
-                print("------- {} {} {}----------".format(deploy, txn_len, runtime))
                 all_count = 0
                 all_ok = 0
                 all_fail = 0
                 all_info = 0
                 all_time = 0
+                all_error = 0
 
                 for res in check_data:
                     # print(res)
@@ -186,21 +224,17 @@ if __name__ == '__main__':
                     all_fail = all_fail + res.fail
                     all_info = all_info + res.info
                     all_time = all_time + res.time
+                    all_error = all_error + res.error
 
                 n = len(check_data)
+                if n == 0:
+                    continue
                 avg_count = all_count / n
                 avg_ok = all_ok / n
                 avg_fail = all_fail / n
                 avg_info = all_info / n
                 avg_time = all_time / n
-                print("Check {} txns, avg_count={}, avg_ok={}, avg_info={}, avg_info={}, avg_time={}".format(n,
-                                                                                                             int(
-                                                                                                                 avg_count),
-                                                                                                             int(
-                                                                                                                 avg_ok),
-                                                                                                             int(
-                                                                                                                 avg_fail),
-                                                                                                             int(
-                                                                                                                 avg_info),
-                                                                                                             int(
-                                                                                                                 avg_time)))
+                avg_error = all_error / n
+                print("------- {} {} {}----------".format(deploy, txn_len, runtime))
+                print("Check {} txns, avg_count={}, avg_ok={}, avg_info={}, avg_info={}, avg_time={}, error={}ms"
+                      .format(n, int(avg_count), int(avg_ok), int(avg_fail), int(avg_info), int(avg_time), round(avg_error, 3)))
